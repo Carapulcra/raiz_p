3282882621236f62c8ae92f01b0ff4f8ad34ac92
Carlos Castro <79557159+Carapulcra@users.noreply.github.com>
2021-12-06 10:21:03 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
-#include <math.h>
-#include <vector>
 using namespace std;
 
-long long exp_mod(long long p, long long g, long long x){
-	long long aux = pow(g,x);
-	return aux%p;
+int mod(int a, int n){
+	if(a>0){
+		int q = a/n;
+		int r = a-(q*n);
+		return r;
+	}
+	else if(a<0){
+		int q = a/n - 1;
+		int r = a - (q*n);
+		return r;
+	}
+	else return 0;
+}
+
+int inversa(int a, int n){
+	int aori = a;
+	int nori = n;
+	int x0=1,y0=0,x=0,y=1;
+  while(n>0){
+  int aux=x;
+  int q=a/n;
+  int r=a%n;
+  x=x0-q*x;
+  x0=aux;
+  aux=y;
+  y=y0-q*y;
+  y0=aux;
+  a=n;
+  n=r;
+  }
+	if(a==1){
+		return mod(x0,nori);
+	}
+	else{
+		cout << aori << " no tiene i.m. en módulo " << nori << endl;
+		return 0;
+	}
 }
 
 int main() {
-	long long p,x,g,y;
-	cin >> p;
-	for(g = 2; g < p; g++){
-		for(x = 1; x < p-1; x++){
-			if(exp_mod(p,g,x) == 1) break;
-		}
-	if(x == p-1) break;
-	}
-	int gmin = g;
-	cout << gmin << endl;
-	vector<long long> Y;
-	for(x = 1; x < p; x++){
-		y = exp_mod(p,gmin,x);
-		Y.push_back(y);
-		cout << gmin <<"^"<< x <<" mod " << p << " = " << y << endl;
-	}
-	cout << "¿" << gmin << " es raíz primitiva?" << endl;
-	if(Y.size() == p-1) cout << "SÍ";
-	else cout << "NO";
+  cout << inversa(15,46);
 } 
